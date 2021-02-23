a2c8c483a60593ddc0991ad340f30029785c8823
Amy-bcx <69481610+amy-bcx@users.noreply.github.com>
2021-02-23 13:08:01 -0600

    Add files via upload

---
+++ build/precache-manifest.5360761881b72fb518e8a34d0d23d4ee.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14ba5ef6dc1f1e3aa18e0846998ed71f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c40efd648edb7fa48d28",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "1f93fba6452cf5f24724",
+    "url": "/static/css/4.2c521bd5.chunk.css"
+  },
+  {
+    "revision": "5205c3c842e46ca8c59f",
+    "url": "/static/css/main.29e23941.chunk.css"
+  },
+  {
+    "revision": "83633929678f8b2ca78f",
+    "url": "/static/js/2.2aaa32c5.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.2aaa32c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40efd648edb7fa48d28",
+    "url": "/static/js/3.5ab9c2d6.chunk.js"
+  },
+  {
+    "revision": "1f93fba6452cf5f24724",
+    "url": "/static/js/4.0fb46604.chunk.js"
+  },
+  {
+    "revision": "5205c3c842e46ca8c59f",
+    "url": "/static/js/main.3ebb5cd7.chunk.js"
+  },
+  {
+    "revision": "d2c19db9a701e223cd8e",
+    "url": "/static/js/runtime-main.f9df9f7f.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "8b6af46bf950615e06ad1fa0df80e1f3",
+    "url": "/static/media/AmanDevBlog.8b6af46b.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "3c76aaab43a10eb3a381a60a0c9a73d2",
+    "url": "/static/media/academieLyonLogo.3c76aaab.jpg"
+  },
+  {
+    "revision": "b0aeaa23f106db069e00c9ecb11f1af1",
+    "url": "/static/media/amanDevLogo.b0aeaa23.jpg"
+  },
+  {
+    "revision": "6bef683e8ed4ce69da8deed9da6a3ba4",
+    "url": "/static/media/contactMailDark.6bef683e.svg"
+  },
+  {
+    "revision": "870326082449ba545c75974b8eab3199",
+    "url": "/static/media/developerActivity.87032608.svg"
+  },
+  {
+    "revision": "efb269bb0804949260701d32a7532b8f",
+    "url": "/static/media/grenobleLogo.efb269bb.png"
+  },
+  {
+    "revision": "0541813768605c0e462093e9bf0a1eca",
+    "url": "/static/media/me.05418137.png"
+  },
+  {
+    "revision": "982d902ff758a793d276ab5d8a120930",
+    "url": "/static/media/me.982d902f.gif"
+  },
+  {
+    "revision": "c079bc45cabf7fe4072017f73cf4f2ab",
+    "url": "/static/media/metaxUSA.c079bc45.png"
+  },
+  {
+    "revision": "1923f70fc6aacaf51bd5e93fada9c4f3",
+    "url": "/static/media/openClassroomsLogo.1923f70f.png"
+  },
+  {
+    "revision": "f063c93deebe2d8982d60d9d5c6579bf",
+    "url": "/static/media/skill.f063c93d.svg"
+  },
+  {
+    "revision": "279907e3032f7355d34f3bde1371f1e4",
+    "url": "/static/media/solidityLogo.279907e3.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
